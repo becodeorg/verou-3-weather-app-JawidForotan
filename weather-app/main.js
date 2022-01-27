2be9338f2da260b9cc5e7dd9a330f5a85b35600b
Jawid <jawidforotan100@gmail.com>
2022-01-27 17:07:21 +0100

    Wrote a function wich gives the city photos in weather app

--- weather-app/main.js
+++ weather-app/main.js
@@ -1,5 +1,5 @@
 const apiKey = "a7838a7fd0899014d0bd24874d68c9ec";
-
+const photoKey = "ITOCDNJoj5saUcLxJkUZ2wIc0UXLZMGUTXup2yf6VO8";
 
 // Create input contents
 const createSearchDiv = document.createElement("div");
@@ -21,36 +21,68 @@ createSearchDiv.append(CreateInput, CreateButton);
 // Create container for html elements
 const createContainer = document.createElement("div");
 createContainer.setAttribute("class", "container");
+
+const createImageCont = document.createElement("div");
+createImageCont.setAttribute("class", "imagCont");
+
+const createWeatherCont = document.createElement("div");
+createWeatherCont.setAttribute("class", "weatherCont");
+
+createContainer.append(createImageCont, createWeatherCont);
+
+const createImgDiv = document.createElement("div");
+createImgDiv.setAttribute("class", "imageDiv");
+createImageCont.appendChild(createImgDiv);
+
+const createImg = document.createElement("img");
+createImg.setAttribute("class", "cityImage");
+createImgDiv.appendChild(createImg);
+
 document.body.appendChild(createContainer);
 
 const createElements = (daily, city, day) => {
   const createWeatherDiv = document.createElement("div");
-  createWeatherDiv.setAttribute("class", "weather");
-  createContainer.appendChild(createWeatherDiv);
+  createWeatherDiv.setAttribute("class", "weatherDiv");
+
+  createWeatherCont.appendChild(createWeatherDiv);
+
   const createName = document.createElement("h3");
   createName.setAttribute("class", "name");
+
   const createDateDiv = document.createElement("div");
   createDateDiv.setAttribute("class", "dateTime");
+
   const createDate = document.createElement("h4");
   createDate.setAttribute("class", "date");
+
   const createTime = document.createElement("h4");
   createTime.setAttribute("class", "time");
+
   createDateDiv.append(createDate, createTime);
+
   const createTemp = document.createElement("h1");
   createTemp.setAttribute("class", "temperature");
+
   const createImg = document.createElement("img");
   createImg.setAttribute("class", "image");
+
   const createMinMaxDiv = document.createElement("div");
   createMinMaxDiv.setAttribute("class", "minMax");
+
   const createMin = document.createElement("p");
   const createMax = document.createElement("p");
+
   createMinMaxDiv.append(createMin, createMax);
+
   const createDesc = document.createElement("p");
   createDesc.setAttribute("class", "description");
+
   const createHumid = document.createElement("p");
   createHumid.setAttribute("class", "humidity");
+
   const createWind = document.createElement("p");
   createWind.setAttribute("class", "wind");
+
   createWeatherDiv.append(
     createName,
     createTemp,
@@ -113,12 +145,27 @@ const getData = () => {
             const date = new Date();
             let day = weekday[(date.getDay() + i) % 7];
             let d = date.toLocaleString();
-            console.log(d);  
+            console.log(d);
             createElements(result.daily[i], city, day);
           }
         });
     });
 };
 
+// Display city image
+async function getCityImage() {
+  const city_name = document.querySelector(".myInput").value;
+  const url =
+    "https://api.unsplash.com/search/photos?query=" +
+    city_name +
+    "&client_id=" +
+    photoKey;
+  const getImage = await fetch(url).then((response) => response.json());
+  const selectImg = document.querySelector(".cityImage");
+  selectImg.src = getImage.results[0].urls.regular;
+}
+
+// Add events to the search button
 const addEventToBtn = document.querySelector(".btn");
 addEventToBtn.addEventListener("click", getData);
+addEventToBtn.addEventListener("click", getCityImage);
